156a729beed717560b122d8998d8fe95ddfe9335
Farhat Momin <143961099+farhattt-1203@users.noreply.github.com>
2024-07-24 13:04:34 +0000

    data types conversion

--- 01_basics/03_conversionOpertions.js
+++ 01_basics/03_conversionOpertions.js
@@ -28,3 +28,38 @@ console.log(typeof age);
 let convertToString = String(age)
 console.log(convertToString);
 console.log(typeof convertToString);
+
+
+
+//  **********Operations**********
+let value = 19
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Hello World"
+let str2 = ", Keep coding"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 9);   // 19
+console.log(1 + "9");   // 19
+console.log("1" + 9 + 9);   // 199
+console.log(1 + 9 + "9");   // 109
+console.log((3+4)*5%3);
+console.log(true);   // true
+console.log(+true);   // 1
+console.log(+"");   // 1
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
