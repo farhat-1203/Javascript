6d21b7dabfb03b04588fc3b0eac03993773c1110
Farhat Momin <143961099+farhattt-1203@users.noreply.github.com>
2024-07-24 07:51:55 +0000

    typecasting

---
+++ 01_basics/03_conversionOpertions.js
@@ -0,0 +1,30 @@
+
+// Converting into Number
+let score = "Farhat"
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// 33abc => NaN
+// true => 1; false => 0
+
+// Converting into Boolean values
+let isLoggedIn = "Farhat"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Farhat" => true
+
+// Number to String
+let age = 19
+console.log(typeof age);
+let convertToString = String(age)
+console.log(convertToString);
+console.log(typeof convertToString);
